0692682b6e4f5ce7daadc4a477967ced2557d3f2
JitenDhawan <jitendhawan@hotmail.com>
2017-03-31 16:08:30 +0530

    Update script.js

--- Code-Fun-Do/script.js
+++ Code-Fun-Do/script.js
@@ -3,18 +3,18 @@ app.controller("myCtrl",function($scope){
 	$scope.bbfirst="LightCyan";
 	$scope.obfirst="OldLace";
 	$scope.ibfirst="Lavender";
-	$scope.pfirst="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/16386969_1767692746887377_1929223042816855817_n.jpg?oh=2dbeb5104ed08d97af502aec29880efa&oe=596FED4F";
-	$scope.nfirst="Jiten Dhawan";
+	$scope.pfirst="https://upload.wikimedia.org/wikipedia/commons/d/d3/Albert_Einstein_Head.jpg";
+	$scope.nfirst="Albert Einstein";
 	$scope.bbsecond="LightCyan";
 	$scope.obsecond="OldLace";
 	$scope.ibsecond="Lavender";
-	$scope.psecond="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/13263894_1059360844130519_3745057438564613740_n.jpg?oh=ee971c452aea76e6c75648c80533a5ad&oe=596064D5";
-	$scope.nsecond="Devashish Mehta";
+	$scope.psecond="https://upload.wikimedia.org/wikipedia/commons/d/d4/N.Tesla.JPG";
+	$scope.nsecond="Nikola Tesla";
 	$scope.bbthird="LightCyan";
 	$scope.obthird="OldLace";
 	$scope.ibthird="Lavender";
-	$scope.pthird="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/15492590_1406208976057309_3084535920846425649_n.jpg?oh=08379d7df2068e3075fdbcb9555a5e25&oe=595E2581";
-	$scope.nthird="Pranjal Prasoon";
+	$scope.pthird="https://upload.wikimedia.org/wikipedia/commons/3/39/GodfreyKneller-IsaacNewton-1689.jpg";
+	$scope.nthird="Isaac Newton";
 });
 setTimeout(function(){
 	alert("Try changing colors, Picture, Name, and have fun.;-)");
